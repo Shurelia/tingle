88c145cc8b261776aad2be3e0b67737e856e6733
Ezra Curry <shurelia@ar-toneli.co>
2017-11-21 15:37:27 -0600

    settimeout on stickyfoot set

--- src/tingle.js
+++ src/tingle.js
@@ -191,7 +191,7 @@
                 this.modal.appendChild(this.modalBoxFooter);
                 this.modalBoxFooter.classList.add('tingle-modal-box__footer--sticky');
                 _recalculateFooterPosition.call(this);
-                this.modalBoxContent.style['padding-bottom'] = this.modalBoxFooter.clientHeight + 20 + 'px';
+                setTimeout(() => { this.modalBoxContent.style['padding-bottom'] = this.modalBoxFooter.clientHeight + 20 + 'px' }, 5);
             }
         } else if (this.modalBoxFooter) {
             if (!this.modalBox.contains(this.modalBoxFooter)) {
